90bc426d3f76a1a135b93876c1f004afabfceefe
Wilfred Hughes <me@wilfred.me.uk>
2018-12-13 00:54:42 +0000

    Configuring eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    amd: true,
+    node: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2015
+  },
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
+};
