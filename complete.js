341a46445addcf37d8d8a5d7e4bc214504b10c3b
Wilfred Hughes <me@wilfred.me.uk>
2019-03-17 23:11:04 +0000

    Wrap in function

--- complete.js
+++ complete.js
@@ -3,37 +3,40 @@ let recast = require("recast");
 let b = recast.types.builders;
 let n = recast.types.namedTypes;
 
-// Let's turn this function declaration into a variable declaration.
-let code = "function add(a, b) {}";
+function parseAndEval(code) {
+  // Parse the code using an interface similar to require("esprima").parse.
+  let ast = recast.parse(code);
 
-// Parse the code using an interface similar to require("esprima").parse.
-let ast = recast.parse(code);
+  // Grab a reference to the function declaration we just parsed.
+  let f = ast.program.body[0];
 
-// Grab a reference to the function declaration we just parsed.
-let f = ast.program.body[0];
+  // Make sure it's a FunctionDeclaration (optional).
+  n.FunctionDeclaration.assert(f);
 
-// Make sure it's a FunctionDeclaration (optional).
-n.FunctionDeclaration.assert(f);
+  function appendReturn(block, id) {
+    let stmts = block.body;
+    stmts = stmts.concat([b.returnStatement(id)]);
 
-function appendReturn(block, id) {
-  let stmts = block.body;
-  stmts = stmts.concat([b.returnStatement(id)]);
+    return b.blockStatement(stmts);
+  }
 
-  return b.blockStatement(stmts);
+  let body = f.body;
+  f.params.forEach(param => {
+    let name = param.name;
+    f.body = appendReturn(body, b.identifier(name));
+
+    let finalCode = recast.print(ast).code + "\nadd(1, 2);";
+
+    // eslint-disable-next-line no-console
+    console.log(finalCode);
+
+    let result = vm.runInNewContext(finalCode);
+
+    // eslint-disable-next-line no-console
+    console.log(result);
+  });
 }
 
-let body = f.body;
-f.params.forEach(param => {
-  let name = param.name;
-  f.body = appendReturn(body, b.identifier(name));
-
-  let finalCode = recast.print(ast).code + "\nadd(1, 2);";
-
-  // eslint-disable-next-line no-console
-  console.log(finalCode);
-
-  let result = vm.runInNewContext(finalCode);
-
-  // eslint-disable-next-line no-console
-  console.log(result);
-});
+// Let's turn this function declaration into a variable declaration.
+let code = "function add(a, b) {}";
+parseAndEval(code);
