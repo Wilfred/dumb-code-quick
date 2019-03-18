9638926aaff996124c07791efe816b18dc3429d9
Wilfred Hughes <me@wilfred.me.uk>
2019-03-18 09:56:58 +0000

    Load expect() explicitly in example test

--- roman_numerals.test.js
+++ roman_numerals.test.js
@@ -1,5 +1,6 @@
 const roman = require("./roman_numerals");
+const expect = require("expect");
 
 test("Roman: I Number: 1", () => {
-  expect(roman("I")).toBe(1);
+  expect(roman.toNumber("I")).toBe(1);
 });
