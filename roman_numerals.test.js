371db36a13473f30ed81d8b73c72c82673fae0b1
Wilfred Hughes <me@wilfred.me.uk>
2019-11-05 10:08:24 +0000

    Make the test harder so we don't use literals

--- roman_numerals.test.js
+++ roman_numerals.test.js
@@ -4,3 +4,7 @@ const expect = require("expect");
 test("Roman: I Number: 1", () => {
   expect(roman.toNumber("I")).toBe(1);
 });
+
+test("Roman: II Number: 2", () => {
+  expect(roman.toNumber("II")).toBe(2);
+});
